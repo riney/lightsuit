e33f6555dd004c4bb7464fa929119e702bad8bbd
John Riney <john@rineysoft.com>
2019-08-08 09:04:22 -0700

    Debugging

--- src/display.js
+++ src/display.js
@@ -11,6 +11,7 @@ const matrix = new LedMatrix(32, 64);
 matrix.clear();
 
 router.get('/fill/:hexColor', (ctx, next) => {
+  console.log(`Received fill command, hexColor: ${ctx.params.hexColor}`);
   const {red, green, blue} = hexRGB(ctx.params.hexColor);
   matrix.fill(red, green, blue);
   matrix.update();
