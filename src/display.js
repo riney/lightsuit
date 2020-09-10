4e2c647de3ff636278afe8731fa1951006e83936
John Riney III <john@rineysoft.com>
2020-09-10 15:39:33 -0700

    Temporary: Using command line image display

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 const exec = require("child_process");
 const hexRGB = require('hex-rgb');
 const Koa = require('koa');
-const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
+//const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
 const Router = require('koa-router');
 
 const PORT = 8080;
@@ -9,18 +9,18 @@ const IMAGE_COMMAND = '/home/pi/lightsuit/scripts/draw_image.sh '
 
 const app = new Koa();
 const router = new Router();
-const matrix = new LedMatrix(32, 64);
-matrix.clear();
+// const matrix = new LedMatrix(32, 64);
+// matrix.clear();
 
-router.get('/fill/:hexColor', (ctx, next) => {
-  console.log(`Received fill command, hexColor: ${ctx.params.hexColor}`);
-  const {red, green, blue} = hexRGB(ctx.params.hexColor);
-  console.log(`Filling with ${red} ${green} ${blue}`)
-  matrix.fill(red, green, blue);
-  console.log('Updating');
-  matrix.update();
-  console.log('Updated display');
-});
+// router.get('/fill/:hexColor', (ctx, next) => {
+//   console.log(`Received fill command, hexColor: ${ctx.params.hexColor}`);
+//   const {red, green, blue} = hexRGB(ctx.params.hexColor);
+//   console.log(`Filling with ${red} ${green} ${blue}`)
+//   matrix.fill(red, green, blue);
+//   console.log('Updating');
+//   matrix.update();
+//   console.log('Updated display');
+// });
 
 router.get('/image/:name', (ctx, next) => {
   console.log(`Received image command, name: ${ctx.params.name}`);
