3f6c3aa8624cd48c767ad5568acb70f48a191c6b
John Riney III <john@rineysoft.com>
2022-07-27 22:26:00 +0000

    Cleanup for v2

--- scripts/image_chest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/home/pi/gitroot/rpi-rgb-led-matrix/utils/led-image-viewer --led-cols=128 --led-rows=32 --led-pixel-mapper="S-mapper;Rotate:180" --led-chain=4 --led-parallel=2 --led-panel-type="FM6126A" /home/pi/$1
--- scripts/image_head.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/home/pi/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 /home/pi/$1
